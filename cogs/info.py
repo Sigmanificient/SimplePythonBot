9613f3239f07ed35f9484f99fdd3ca33fa99deaa
Sigmanificient <edhyjox@gmail.com>
2021-05-16 23:04:32 +0200

    user info command

--- cogs/info.py
+++ cogs/info.py
@@ -1,4 +1,6 @@
 from time import perf_counter
+
+import discord
 from discord.ext import commands
 
 
@@ -60,6 +62,53 @@ class Utils(commands.Cog):
             _embed.add_field(name=f"{k} latency", value=f'> `{v * 1e3:,.2f} ms`')
 
         await ping_message.edit(content=' ', embed=_embed)
+
+    @commands.command(
+        name="user",
+        aliases=("whois", "more"),
+        brief="return the user information"
+    )
+    async def user(self, ctx, user: discord.Member = None):
+        """ return the user information """
+        if user is None:
+            user = ctx.author
+
+        embed = self.client.embed(color=user.color)
+        embed.set_thumbnail(url=user.avatar_url)
+
+        embed.set_author(
+            name=f"{user.name}%s Information" % ("'" if user.name.endswith("s") else "'s"),
+            icon_url=user.avatar_url
+        )
+
+        if user.nick is not None:
+            embed.add_field(name="> nickname", value=f"`{user.nick}`")
+
+        for name, value in {
+            "default avatar": user.default_avatar,
+            "platform": 'Mobile' if user.is_on_mobile() else 'computer',
+            "status": user.status,
+            "discriminator": user.discriminator,
+            "created date": user.created_at.strftime('%d %b. %Y'),
+            "joined date": user.joined_at.strftime('%d %b. %Y'),
+        }.items():
+            embed.add_field(name=f'> {name}', value=f'`{value}`')
+
+        if user.activity is not None:
+            embed.add_field(name=f'> Activity', value=user.activity.name)
+
+        if len(user.roles) < 50:
+            roles = ", ".join([f"{role.name}" for role in user.roles[:0: -1]])
+        else:
+            roles = "too many roles"
+
+        embed.add_field(
+            name="> roles (%i)" % (len(user.roles) - 1),
+            value=f"```c\n{roles} ```",
+            inline=False
+        )
+
+        await ctx.send(embed=embed)
 
 
 def setup(client):
